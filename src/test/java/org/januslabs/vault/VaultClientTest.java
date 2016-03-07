b616d070a4b0bfcb346d76594d40a502db552ae8
nandhusriram <nandhusriram@gmail.com>
2016-03-06 21:00:28 -0600

    updated readme

--- src/test/java/org/januslabs/vault/VaultClientTest.java
+++ src/test/java/org/januslabs/vault/VaultClientTest.java
@@ -17,29 +17,7 @@ public class VaultClientTest {
   public static String VAULT_TOKEN="e549db34-8733-6a15-8847-9d079ec76b0f";
   private @Autowired VaultClient vaultclient;
 
- /* @Before
-  public void before() throws Exception {
-    
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-    context.registerShutdownHook();
-    Environment environment = context.getBean(Environment.class);
-    String token = environment.getProperty("vault.token").toString();
-    System.out.println(token);
-   
-  }
- 
-  
-  @PropertySource("classpath:application.properties")
-  static
-  class AppConfig {
-
-      @Bean
-      public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-          return new PropertySourcesPlaceholderConfigurer();
-      }
-  }*/
-  
- @Test
+  @Test
   public void testVaultClient()
   {
  
